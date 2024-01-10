04fe911bc44db6b8ca2ae5ae66cc536ef79f75f4
Jung Hun Soo <hunsoodev@gmail.com>
2024-01-10 22:41:26 +0900

    fix: Resolve data extraction error

--- dags/Recent30DaysStockData.py
+++ dags/Recent30DaysStockData.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
 from airflow.models import Variable
 
@@ -12,6 +11,11 @@ from datetime import timedelta
 import pandas as pd
 import requests
 import logging
+import pendulum
+import time
+
+## 로컬 타임존 생성
+local_tz = pendulum.timezone("Asia/Seoul")
 
 SNOWFLAKE_CONN_ID = 'hunsoo_snowflake_conn'
 SNOWFLAKE_STAGE = 'hunsoo_external_stage'
@@ -20,7 +24,7 @@ SNOWFLAKE_ROLE = 'ACCOUNTADMIN'
 SNOWFLAKE_DATABASE = 'HUNSOO'
 SNOWFLAKE_SCHEMA = 'RAW_DATA'
 SNOWFLAKE_TABLE = 'stock_prices_daily'
-S3_FILE_PATH = 's3://programmers-bucket/hunsoo/get_stock_prices_daily.csv'
+S3_FILE_NAME = 'get_stock_prices_daily.csv'
 
 
 def create_url():
@@ -29,48 +33,60 @@ def create_url():
     return f"{BASE_URL}/{PATH}"
 
 
-def extract_task(ACCESS_TOKEN, API_KEY, API_SECRET):
-    try:
-        URL = create_url()
-        
-        # 헤더 설정
-        headers = {
-            "Content-Type": "application/json", 
-            "authorization": f"Bearer {ACCESS_TOKEN}",
-            "appKey": API_KEY,
-            "appSecret": API_SECRET,
-            "tr_id": "FHKST01010400"}
+def extract_task():
+    logging.info("Starting data extraction process")
+    APP_KEY = Variable.get("APP_KEY")
+    APP_SECRET = Variable.get("APP_SECRET")
+    ACCESS_TOKEN = Variable.get("ACCESS_TOKEN")
 
-        params = {
-            "fid_cond_mrkt_div_code": "J",  # 주식
-            "fid_input_iscd": '005380',     # 종목 코드(임시) stock_no
-            "fid_period_div_code": "D",     # 최근 30거래일
-            "fid_org_adj_prc": "0",         # 수정주가반영
-        }
+    max_attempts = 3  # 최대 시도 횟수
+    attempt = 0  # 현재 시도 횟수
+    URL = create_url()
+    
+    # 헤더 설정
+    headers = {
+        "Content-Type": "application/json", 
+        "authorization": f"Bearer {ACCESS_TOKEN}",
+        "appKey": APP_KEY,
+        "appSecret": APP_SECRET,
+        "tr_id": "FHKST01010400"}
 
-        logging.info("Requesting data from URL")
-        
-        # 호출
-        response = requests.get(URL, headers=headers, params=params)
+    params = {
+        "fid_cond_mrkt_div_code": "J",  # 주식
+        "fid_input_iscd": '005380',     # 종목 코드(임시) stock_no
+        "fid_period_div_code": "D",     # 최근 30거래일
+        "fid_org_adj_prc": "0",         # 수정주가반영
+    }
 
-        if response.status_code == 200 and response.json()["rt_cd"] == "0":  # 0: 성공
-            json_data = response.json()
-            df_data = pd.DataFrame(json_data['output'])
-            logging.info("Data extraction successful")
-            return (df_data)
-        else:
-            logging.error(f"Failed to retrieve data: {response.status_code}, {response.json()}")
-            return None
-            
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+    logging.info("Requesting data from URL")
+    
+    while attempt < max_attempts:
+        logging.info(f"{attempt+1}/{max_attempts} 시도")
+        try:
+            response = requests.get(URL, headers=headers, params=params)
+            if response.status_code == 200 and response.json()["rt_cd"] == "0":
+                logging.info("Data extraction successful")
+                return response.json()
+            else:
+                print("요청 실패, 상태 코드:", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("HTTP 요청 중 오류 발생:", e)
+
+        attempt += 1
+        time.sleep(1)  # 1초 대기
+
+    # 호출
+    if attempt == max_attempts:
+        logging.error("Data extraction failed")
         raise
+            
 
 
 def transform_task(**context):
     logging.info("Starting data transformation process")
 
-    df = context["task_instance"].xcom_pull(key="return_value", task_ids="extract")
+    json_data = context["task_instance"].xcom_pull(key="return_value", task_ids="extract")
+    df = pd.DataFrame(json_data['output'])
     logging.info("Data extraction completed")
 
     df['stck_bsop_date'] = df['stck_bsop_date'].apply(lambda x: f"{x[:4]}-{x[4:6]}-{x[6:]}")
@@ -92,7 +108,10 @@ def transform_task(**context):
     return df
 
 
-def upload_df_to_s3(bucket_name, s3_key, **context):
+def upload_df_to_s3(**context):
+    bucket_name = context["params"]["bucket_name"]
+    s3_key = context["params"]["s3_key"]
+
     df = context["task_instance"].xcom_pull(key="return_value", task_ids="transform")
     
     try:
@@ -117,7 +136,7 @@ def upload_df_to_s3(bucket_name, s3_key, **context):
 dag = DAG(
     dag_id="get_stock_prices_daily",
     tags=['dev-course'],
-    start_date=datetime(2024, 1, 10),
+    start_date=datetime(2024, 1, 9, tzinfo=local_tz),
     schedule="@daily",
     catchup=False,
     max_active_runs=1,
@@ -131,11 +150,6 @@ dag = DAG(
 extract = PythonOperator(
     task_id = 'extract',
     python_callable = extract_task,
-    op_kwargs = {
-        'ACCESS_TOKEN': Variable.get("ACCESS_TOKEN"),
-        'API_KEY': Variable.get("API_KEY"),
-        'API_SECRET': Variable.get("API_SECRET")
-    },
     dag = dag
 )
 
@@ -151,7 +165,7 @@ upload_S3 = PythonOperator(
     task_id='upload_df_to_s3',
     python_callable=upload_df_to_s3,
     provide_context=True,
-    op_kwargs={
+    params={
         'bucket_name': 'programmers-bucket',
         's3_key': 'hunsoo/get_stock_prices_daily.csv'
     },
@@ -161,7 +175,7 @@ upload_S3 = PythonOperator(
 copy_into_table = S3ToSnowflakeOperator(
     task_id='copy_into_table',
     snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    s3_keys=[S3_FILE_PATH],
+    s3_keys=[S3_FILE_NAME],
     table=SNOWFLAKE_TABLE,
     schema=SNOWFLAKE_SCHEMA,
     stage=SNOWFLAKE_STAGE,
