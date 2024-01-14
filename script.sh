fd463152bae72ce77f4518434e299f5e8935f9fe
Hun Soo Jeong <147886939+hunsoodev@users.noreply.github.com>
2024-01-15 07:30:38 +0900

    Update script.sh
    develop -> main

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ echo "Moving to the project directory..."
 cd ~/dev-course-project-03 || { echo "Failed to change directory. Exiting."; exit 1; }
 
 echo "Switching to 'develop' branch..."
-git switch develop || { echo "Failed to switch branch. Exiting."; exit 1; }
+git switch main || { echo "Failed to switch branch. Exiting."; exit 1; }
 
 echo "Fetching the latest changes from origin..."
 git fetch || { echo "Failed to fetch changes. Exiting."; exit 1; }
@@ -15,7 +15,7 @@ current_branch=$(git branch --show-current)
 echo "Current branch is: $current_branch"
 
 echo "Checking for changes in 'docker-compose.yaml'..."
-if git diff HEAD..origin/develop --name-only | grep -q 'docker-compose.yaml'; then
+if git diff HEAD..origin/main --name-only | grep -q 'docker-compose.yaml'; then
     echo "'docker-compose.yaml' has changed."
     echo "Stopping and restarting docker-compose services..."
     docker-compose down && docker-compose up -d || { echo "Failed to restart docker-compose services. Exiting."; exit 1; }
